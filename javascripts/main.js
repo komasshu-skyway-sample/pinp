ff9ff86b092d1ac333aea4f2acbd16d8072eda06
kensakukomatsu <kensaku.komatsu@gmail.com>
2014-02-02 13:07:41 +0900

    for issue #2 preserve aspect ration when PnP resized

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,42 +1,48 @@
-var $div;
 
-function startPinP(){
-  $div = $("<div>")
-    .attr("draggable", true)
-    .css({
-      "position": "absolute",
-      "top": "10px",
-      "left": "0",
-      "z-index": 9999,
-      "width": "160px",
-      "height": "120px",
-      "border": "1px dashed #ccc"
-    })
-    .PinP()
-    .draggable()
-    .resizable()
-    .appendTo("body");
-}
+(function(global){
+  var startFlag = false // false if PinP doesn't start
+    , $div    // div node displays PinP
+    , WIDTH = 160  // width of PinP frame
+    , HEIGHT = 120 // height of PinP frame
+    , ASPECT = WIDTH / HEIGHT // aspect ratio of PinP frame
 
-var startFlag = false;
-
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    switch(request.op) {
-    case "start":
-      if(!startFlag) {
-        startFlag = true;
-        startPinP();
-      } else {
-        $div.trigger("start");
-      }
-      break;
-    case "stop":
-      $div.trigger("stop");
-      break;
-    default:
-      break;
-    }
-    sendResponse("message received");
+  function startPinP(){
+    $div = $("<div>")
+      .attr("draggable", true)
+      .css({
+        "position": "absolute",
+        "top": "10px",
+        "left": "0",
+        "z-index": 9999,
+        "border": "1px dashed #ccc"
+      })
+      .width(WIDTH)
+      .height(HEIGHT)
+      .PinP()
+      .draggable()
+      .resizable({aspectRatio: ASPECT})
+      .appendTo("body");
   }
-);
+
+
+  chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+      switch(request.op) {
+      case "start":
+        if(!startFlag) {
+          startFlag = true;
+          startPinP();
+        } else {
+          $div.trigger("start");
+        }
+        break;
+      case "stop":
+        $div.trigger("stop");
+        break;
+      default:
+        break;
+      }
+      sendResponse("message received");
+    }
+  );
+}(window));
